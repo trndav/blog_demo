34d01229a4852ba73b668a0b9c62634ddfbfc2c9
trndav <farsiteguide@gmail.com>
2023-10-07 10:01:23 +0200

    Added view to posts ˛bootstrap JS, dropdown functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_05_143231) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_06_161652) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "views", default: 0
   end
 
 end
