3df2231d7e0497af337e428aaa75cf0169d1cc7b
trndav <farsiteguide@gmail.com>
2023-10-09 10:49:10 +0200

    Added user to post association

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
     validates :title, presence: true, length: {minimum: 3, maxmum: 50}
-    validates :body, presence: true, length: {minimum: 3, maxmum: 200}
+    validates :body, presence: true, length: {minimum: 3, maxmum: 300}
+    belongs_to :user
 end
