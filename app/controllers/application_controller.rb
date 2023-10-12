b8fabd276375f337b36221c3a366c681c1ac7a2c
trndav <farsiteguide@gmail.com>
2023-10-12 12:46:34 +0200

    Comments on post notification functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    before_action :set_notifications, if: :current_user
+
+    private
+    def set_notifications
+        # .newest_first is same like .where().order(created_at: :desc)
+        notifications = Notification.where(recipient: current_user).newest_first.limit(9)
+        @unread = Notification.unread
+        @read = Notification.read
+    end
 end
