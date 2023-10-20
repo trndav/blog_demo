a0b3417abb596d0375ee561c689edbc905a257c6
trndav <farsiteguide@gmail.com>
2023-10-20 10:23:36 +0200

    Fix comments import in seed with recursive: true

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,30 +3,32 @@
 # IRB: rails db:setup
 # Comments are not created bc of:
 # Active record - import problem: Recursive calls only world with postgres.
+# Fixed comments import with recursive: true
 
-# arays to bulk push later
-posts = []
-comments = []
-# measure the elapsed time it takes to execute a block of code
-elapsed = Benchmark.measure do
         User.create(email: "janedoe@email.com", name: "Jane Doe", 
                 password: "123456", password_confirmation: "123456", role: User.roles[:admin])
         User.create(email: "bob@email.com", name: "Bob", 
                 password: "123456", password_confirmation: "123456")
-        50.times do |x|
+
+# comments = []
+# measure the elapsed time it takes to execute a block of code
+elapsed = Benchmark.measure do  
+        # aray to bulk push later      
+        posts = []
+        100.times do |x|
                 puts "Creating post #{x}"
-                post = Post.new(title: "Title #{x}", body: "Body #{x} Words go here Idk", user_id: User.first.id)
-                # add element to array
-                posts.push(post)                                        
+                post = Post.new(title: "Title #{x}", body: "Body #{x} Words go here Idk", user: User.first)
+                # add element to array                                                      
                         2.times do |y|
                         puts "Creating comment #{y} for post #{x}"
-                        comment = post.comments.new(body: "Comment #{y}", user_id: User.second.id)
-                        # add element to array
-                        comments.push(comment)
+                        post.comments.build(body: "Comment #{y}", user: User.second)
+                        # comments.push(comment)
                 end
+                # add element to array
+                posts.push(post)  
         end
+        # bulk push all arrays after all is finished
+        Post.import(posts, recursive: true)
 end
-# bulk push all arrays after all is finished
-Post.import(posts)
-Comment.import(comments)
+# Comment.import(comments)
 puts "Elapsed in #{elapsed.real} seconds."
